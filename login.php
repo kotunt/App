6ec9924d2427628ef469e2ffbf91270ea903fde1
kotunt <tunkyaw00088@gmail.com>
2026-06-27 08:20:45 +0000

    app

--- login.php
+++ login.php
@@ -4,8 +4,8 @@
 // 1. Bootstrap the application to set up the environment
 require_once __DIR__ . '/bootstrap.php';
 
-// 2. Use the controller. The autoloader from bootstrap.php will handle including the file.
-use App\Controllers\LoginController;
+// 2. Instantiate and run the controller.
+require_once __DIR__ . '/src/controllers/LoginController.php';
 
 $controller = new LoginController();
 $controller->handleRequest();
